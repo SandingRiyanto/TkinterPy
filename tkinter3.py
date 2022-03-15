ef8c9b4bdff70f545ba6e7e6fc21b3249a27497f
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-03-15 21:43:45 +0700

    ttk widgets awal

---
+++ tkinter3.py
@@ -0,0 +1,12 @@
+# Ttk Widgets
+import tkinter as tk
+from tkinter import ttk
+
+hai = tk.Tk()
+
+tk.Label(hai, text="sanding").pack()
+ttk.Label(hai, text="sanding").pack()
+
+hai.mainloop()
+
+# next widget ada di pembahasan berikutnya
