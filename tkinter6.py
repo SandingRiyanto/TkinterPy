21cd3bab34e3e662e06f54de5a9a011d406d2de3
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-03-16 13:35:44 +0700

    event bind tkinter

--- tkinter6.py
+++ tkinter6.py
@@ -1,4 +1,3 @@
-# Tkinter Event Binding
 import tkinter as tk
 from tkinter import ttk
 
@@ -7,10 +6,15 @@ def return_pressed(event):
     print('Return key pressed.')
 
 
+def log(event):
+    print(event)
+
+
 root = tk.Tk()
 
 btn = ttk.Button(root, text='Save')
 btn.bind('<Return>', return_pressed)
+btn.bind('<Return>', log, add='+')
 
 
 btn.focus()
