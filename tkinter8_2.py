322089c3da1c74020236f02db457eed121e86620
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-03-17 12:34:13 +0700

    button with image

---
+++ tkinter8_2.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+# root window
+root = tk.Tk()
+root.geometry('600x400')
+root.resizable(False, False)
+root.title('Image Button Demo')
+
+def download_kliked():
+    showinfo(title='informasi', message='download klik!')
+
+donlot_ikon = tk.PhotoImage(file='images/down-arrow.png')
+donlot_button = ttk.Button(root, image=donlot_ikon, command=download_kliked)
+
+donlot_button.pack(
+    ipadx=5,
+    ipady=5,
+    expand=True
+)
+
+
+root.mainloop()
